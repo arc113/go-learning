706d278db823547817bcb659116afd883a65b687
arc113 <archit.bohra@innoraft.com>
2020-10-11 01:37:04 +0530

    updates

--- second.go
+++ second.go
@@ -3,8 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var1 := "Hey, I am archit"
+	var1 := "A-B-"
 	mynum := 13
 	var2 := fmt.Sprintf("I like cookies and my birthday falls on %dth August\n", mynum)
-	fmt.Printf("%q AND %q", var1, var2)
+	fmt.Printf("%x AND %s", var1, var2)
+	fmt.Println(var1)
 }
---
+++ third-function-return-multiple.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	a, b := "First", "Second"
+	var c, d string = "Third", "Fourth"
+
+	fmt.Println(a, b, c, d)
+}
